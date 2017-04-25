c1807bb4ec9cda4b1cb1bfb7e38e77b64ba201a9
Glen Baker <iepathos@gmail.com>
2017-04-25 16:52:13 -0400

    Updated with requirements.txt and PyML

--- sentiment.py
+++ sentiment.py
@@ -176,27 +176,114 @@ class Sentiment:
         # Train SVM classifier
         # self.train_svm()
         return total_result_hash
-    
+
+    def get_objectivity(self, clean_text_areas):
+        """
+            Analysis of text regions using the following order: Each sentence per
+            region is passed from the subjectivity classification using bootstrapping
+            method and then if it turns out to be subjective it is passed 
+            from the polarity classification using bootstrapping method also.
+            Finally, it results to a decision for the sentiment of the sentence
+            and the overall sentiment of the regions. 
+        """
+        if len(clean_text_areas) > 0:
+            for clean_text in clean_text_areas:
+                # Sentence detection
+                clean_text = self.normalize(clean_text)
+                try:
+                    sentences = self.sentence_tokenizer.tokenize(clean_text)
+                except:
+                    return {}
+                sentiments = []
+                scores = []
+                nscores = []
+                results = {'positive':{'count' : 0, 'score' : 0, 'nscore' : 0},
+                           'neutral':{'count' : 0, 'score' : 0, 'nscore' : 0},
+                           'negative':{'count' : 0, 'score' : 0, 'nscore' : 0}}
+
+                # print
+                # print Tcolors.ACT + " Checking block of text:"
+                # for i, sentence in enumerate(sentences):
+                #     print "[" + str(i+1) + "] " + sentence
+                for i, sentence in enumerate(sentences):
+                    # Proceed to subjectivity classification (bootstrapping procedure).
+                    # (This step could be skipped in case you deal with subjective sentences only.)
+                    sentiment = ""
+                    previous = ""
+                    next = ""
+                    score = 0
+                    nscore = 0
+                    if i == 0 and i + 1 < len(sentences): 
+                        next = sentences[i+1] 
+                    elif i != 0 and i < len(sentences):
+                        if i + 1 != len(sentences):
+                            next = sentences[i+1]
+                        previous = sentences[i-1] 
+                     
+                    if DEBUG: print Tcolors.ACT + " Analyzing subjectivity..." 
+                    
+                    result = self.bootstrapping.classify(sentence, previous, next) 
+                    if result is None:
+                        res = 'Not found!'
+                    else:
+                        res = result
+                    if DEBUG:
+                        print Tcolors.RES + Tcolors.OKGREEN + " " + res + Tcolors.ENDC
+                        print
+
+                    # If sentence is subjective 
+                    if result == 'subjective' or result is None:
+                        # Proceed to polarity classification
+                        if DEBUG: print Tcolors.ACT + " Analyzing sentiment..."
+                        polarity_classifier = PolarityClassifier(self.pos_tagger, self.lexicon, debug=DEBUG)
+                        sentiment, score, nscore = polarity_classifier.classify(sentence)
+                        if DEBUG: print Tcolors.RES + Tcolors.OKGREEN + " " + sentiment + Tcolors.ENDC
+                    # If sentence is objective
+                    elif result == 'objective':
+                        sentiment = 'neutral'  
+                    # Collect high-confidence training instances for SVM classifier.
+                    # After the training, SVM can be used to classify new sentences.
+                    #if sentiment != "neutral" and sentiment != "": 
+                        #if sentiment != "neutral" and abs(nscore) >= 0.4:
+                        #   self.total_sentences.append(sentence)
+                        #   self.total_sentiments.append(sentiment)
+
+                    # Store results to memory
+                    sentiments.append(sentiment)
+                    scores.append(score)
+                    nscores.append(nscore)
+
+                    # Update score
+                    if results.has_key(sentiment):
+                        results[sentiment]['nscore'] += nscore
+                        results[sentiment]['score'] += score
+                        results[sentiment]['count'] += 1 
+
+                pcount = results['positive']['count']
+                ncount = results['negative']['count'] 
+                total = len(sentences)
+                print float("%.2f" % (100 - ((float)(pcount + ncount)*100 / total)))
+
     def normalize(self, text):
         """
             Make some word improvements before feeding to the sentence tokenizer.
-        """  
+        """
         rr = RepeatReplacer(self.lexicon)
         normalized_text = []
         final = None
         try:
             for word in text.split():
-                normal = rr.replace(word.lower()) 
-                if word[0].isupper(): 
+                normal = rr.replace(word.lower())
+                if word[0].isupper():
                     normal = normal[0].upper() + normal[1:]
-                
+
                 normalized_text.append(normal)
                 final = " ".join(normalized_text)
         except:
                 final = text
-    
+
         return final
-                
+
     def train_svm(self):
         """
             Train SVM and store data with pickle.
@@ -210,9 +297,9 @@ class Sentiment:
         l_output.close()
 
 
-if __name__ == '__main__':       
+if __name__ == '__main__':
     sentiment = Sentiment()
     if len(sys.argv) > 1:
-        sentiment.analyze([sys.argv[1]]) 
+        sentiment.get_objectivity([sys.argv[1]]) 
     else:
-        sentiment.analyze([u"I was blown away by some of the comments here posted by people who is either uneducated, ignorant, self-righteous or all-of-the-above...I'm irritated and saddened as I read these \"finger-pointing\" or \"I'm right and you're wrong\" type of posts! Grow up folks! You're not in grade school...learn to embrace what is positive and move forward to do what is right... I have to give much love and respect to Ronny...your work is AMAZING!!! You cannot fathom how good I feel after I watched this video...regardless of history, politics, or whatever forces that makes what the mid-east today...for what you did and many of the followers in Iran and Palestine ...I BELIEVE TOMORROW WILL BE BETTER!!!!!! My name is Christopher Lee, I'm a nurse in Los Angeles and I {HEART} YOU ALL (especially to all of you beautiful and sweet ladies across the way)!!!!!"])      
+        sentiment.get_objectivity([u"I was blown away by some of the comments here posted by people who is either uneducated, ignorant, self-righteous or all-of-the-above...I'm irritated and saddened as I read these \"finger-pointing\" or \"I'm right and you're wrong\" type of posts! Grow up folks! You're not in grade school...learn to embrace what is positive and move forward to do what is right... I have to give much love and respect to Ronny...your work is AMAZING!!! You cannot fathom how good I feel after I watched this video...regardless of history, politics, or whatever forces that makes what the mid-east today...for what you did and many of the followers in Iran and Palestine ...I BELIEVE TOMORROW WILL BE BETTER!!!!!! My name is Christopher Lee, I'm a nurse in Los Angeles and I {HEART} YOU ALL (especially to all of you beautiful and sweet ladies across the way)!!!!!"])      
