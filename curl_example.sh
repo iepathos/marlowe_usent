4a4cc4bfb02a20211a2d5af321da8db26b31e22e
Glen Baker <iepathos@gmail.com>
2017-04-27 00:25:12 -0400

    Example adjustments

--- curl_example.sh
+++ curl_example.sh
@@ -4,6 +4,8 @@ curl --data "text=100" 0.0.0.0:5000
 
 curl --data "text=This is some random text." http://127.0.0.1:8000 
 
-curl --data "text=This is some random text." https://morning-harbor-96808.herokuapp.com
+curl --data "text=This extension analyzes the objectivity of given text." https://morning-harbor-96808.herokuapp.com
 
-curl --data "text=This is some random text." https://desolate-bastion-86556.herokuapp.com
+curl --data "text=This extension analyzes the objectivity of given text." https://desolate-bastion-86556.herokuapp.com
+
+
