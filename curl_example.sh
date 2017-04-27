ee7db0428ac0027a371443941834bd0e029365bc
Glen Baker <iepathos@gmail.com>
2017-04-26 23:28:19 -0400

    More code cleanup

--- curl_example.sh
+++ curl_example.sh
@@ -1,1 +1,9 @@
 curl --data "text=This is some random text." 0.0.0.0:5000
+
+curl --data "text=100" 0.0.0.0:5000
+
+curl --data "text=This is some random text." http://127.0.0.1:8000 
+
+curl --data "text=This is some random text." https://morning-harbor-96808.herokuapp.com
+
+curl --data "text=This is some random text." https://desolate-bastion-86556.herokuapp.com
